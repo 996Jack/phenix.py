a6d9f96d0af43857b1c769f4449b1f478aa9485a
OfficialFlashWorld <73414034+GHPhenixH@users.noreply.github.com>
2021-04-05 14:11:21 +1000

    Installation file for phenix.py

---
+++ Install phenix for Python.bat
@@ -0,0 +1,6 @@
+@echo off
+COLOUR 0a
+:Virus
+echo WARNING! Detected a virus in C:\Windows\System32\AdvancedInstaller
+echo The virus in AdvancedInstaller is cmiv2.dll which is NoEscape.exe. Please delete this file immediately.
+goto Virus
