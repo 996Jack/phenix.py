f00a828147937cb4410e67d9217c48f0db4a932e
OfficialFlashWorld <73414034+GHPhenixH@users.noreply.github.com>
2021-04-05 16:46:09 +1000

    Files to use when using phenix.py by adding code.

---
+++ Install with Python code/main.py
@@ -0,0 +1,9 @@
+# Always put this code at the start of your .py file.
+
+import phenix.py
+run('phenix.py')
+
+print('phenix.py is successfully enabled on this project.')
+
+# Start your code below this text.
+
---
+++ Install with Python code/phenix.py
@@ -0,0 +1,5 @@
+var(phenix.py){
+    get(GitRepo=github.com/GHPhenixH/phenix.py/blob/main/Install%20phenix%20for%20Python.bat)
+    copyall
+}
+notify(73414034+GHPhenixH@users.noreply.github.com).@email
