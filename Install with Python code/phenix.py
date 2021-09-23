7ce487f92443b099e5639b01871e65c3455a0017
phenix.html <minecraftfuntime24@gmail.com>
2021-09-23 17:21:56 +1000

    Update phenix.py

--- Install with Python code/phenix.py
+++ Install with Python code/phenix.py
@@ -1,5 +1,5 @@
 var(phenix.py){
-    get(GitRepo=github.com/GHPhenixH/phenix.py/blob/main/Install%20phenix%20for%20Python.bat)
+    get(GitRepo=github.com/996Jack/phenix.py/blob/main/Install%20phenix%20for%20Python.bat)
     copyall
 }
-notify(73414034+GHPhenixH@users.noreply.github.com).@email
+notify(73414034+996JackH@users.noreply.github.com).@email
